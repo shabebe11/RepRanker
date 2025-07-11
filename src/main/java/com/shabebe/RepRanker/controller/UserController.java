d51e285f32773ca8fcc8b8153bef5155e4f20bd8
shabebe11 <salk147@aucklanduni.ac.nz>
2025-07-11 19:11:26 +1200

    Added util to calcualte the rank of men and womens lifts for bench, deadlift, and squat and give them a rank

--- src/main/java/com/shabebe/RepRanker/controller/UserController.java
+++ src/main/java/com/shabebe/RepRanker/controller/UserController.java
@@ -3,7 +3,10 @@ package com.shabebe.RepRanker.controller;
 import com.shabebe.RepRanker.dto.UserInputDto;
 import com.shabebe.RepRanker.entity.User;
 import com.shabebe.RepRanker.repository.UserRepository;
+import com.shabebe.RepRanker.util.RanksMen;
+import com.shabebe.RepRanker.util.RanksWomen;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,8 +19,11 @@ public class UserController {
 
   @Autowired private UserRepository userRepository;
 
+  private RanksMen ranksMen;
+  private RanksWomen ranksWomen;
+
   @PostMapping("/submit")
-  public ResponseEntity<String> submitUser(@RequestBody UserInputDto userInput) {
+  public ResponseEntity<User> submitUser(@RequestBody UserInputDto userInput) {
 
     int weight = Math.round(userInput.getBodyweight() / 5f) * 5;
 
@@ -34,6 +40,16 @@ public class UserController {
 
     userRepository.save(user);
 
-    return ResponseEntity.ok("User submitted successfully.");
+    if (userInput.getSex().toLowerCase().equals("male")) {
+      String benchRank = ranksMen.classifyLift("bench", weight, userInput.getBench());
+      String squatRank = ranksMen.classifyLift("squat", weight, userInput.getSquat());
+      String deadliftRank = ranksMen.classifyLift("deadlift", weight, userInput.getDeadlift());
+    } else {
+      String benchRank = ranksWomen.classifyLift("bench", weight, userInput.getBench());
+      String squatRank = ranksWomen.classifyLift("squat", weight, userInput.getSquat());
+      String deadliftRank = ranksWomen.classifyLift("deadlift", weight, userInput.getDeadlift());
+    }
+
+    return new ResponseEntity<>(user, HttpStatus.CREATED);
   }
 }
