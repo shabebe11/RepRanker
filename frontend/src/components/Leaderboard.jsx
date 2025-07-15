b81da880ba509a326bf8b1a77583bf3e58d5be8f
hajunk05 <hajunk05@gmail.com>
2025-07-16 01:54:18 +1200

    Ranking page rendering after submission

--- frontend/src/components/Leaderboard.jsx
+++ frontend/src/components/Leaderboard.jsx
@@ -3,8 +3,6 @@ import '../styles/Leaderboard.css';
 
 const Leaderboard = () => {
 
-    axios.post('http://localhost:8080/api/users/leaderboard', submittedPerson);
-
   return (
     <div className="p-3">
       <h1 className="text-center">Leaderboard</h1>
--- frontend/src/components/SubmitForm.jsx
+++ frontend/src/components/SubmitForm.jsx
@@ -17,9 +17,16 @@ const SubmitForm = () => {
   const [newSquat, setNewSquat] = useState(0);
   const [newDeadlift, setNewDeadlift] = useState(0);
 
+  const [isSubmitted, setIsSubmitted] = useState(true);
+  const [submittedPerson, setSubmittedPerson] = useState({});
+
   const handleSubmit = (event) => {
-    event.preventDefault();
-    const submittedPerson = {
+    event.preventDefault()
+
+    //Remove form and specify ranking of user
+    setIsSubmitted(!isSubmitted)
+
+    const personToSubmit = {
       nickname: newNickname,
       sex: sex,
       weight: newWeight,
@@ -28,100 +35,110 @@ const SubmitForm = () => {
       deadlift: newDeadlift
     };
 
-    console.log(submittedPerson);
-    axios.post('http://localhost:8080/api/users/submit', submittedPerson);
+    setSubmittedPerson(personToSubmit)
+
+    // axios.post('http://localhost:8080/api/users/submit', personToSubmit);
 
     // Reset form
     setSex('');
     setNewNickname('');
     setNewWeight(0);
-    setNewBench(5);
-    setNewSquat(5);
-    setNewDeadlift(5);
+    setNewBench(0);
+    setNewSquat(0);
+    setNewDeadlift(0);
   };
 
-  return (
-    <div className="submit-form-container">
-      <h1>!! Find Your Rank !!</h1>
-      
-      <form onSubmit={handleSubmit}>
-        <div className="form-section">
-          <h2><Form.Label>Nickname</Form.Label></h2>
-          <Form.Control 
-            placeholder="Enter a name"
-            value={newNickname} 
-            onChange={(e) => setNewNickname(e.target.value)}
-          />
-        </div>
+  if (isSubmitted) {
+    return(
+        <div style={{border: "1px solid red"}} >
 
-        <div className="form-section">
-          <h2><Form.Label>Your Weight</Form.Label></h2>
-          <Form.Control 
-            type="number"
-            placeholder="Enter a number"
-            value={newWeight}
-            onChange={(e) => setNewWeight(e.target.value)} 
-          />
         </div>
+    )
+  } else {
+    return (
+        <div className="submit-form-container">
+          <h1>!! Find Your Rank !!</h1>
 
-        <div className="form-section">
-          <h2><Form.Label>Sex</Form.Label></h2>
-          <div className="radio-group">
-            <Form.Check
-              type="radio"
-              label="Male"
-              checked={sex === "male"}
-              onChange={(e) => setSex("male")}
+          <form onSubmit={handleSubmit}>
+            <div className="form-section">
+              <h2><Form.Label>Nickname</Form.Label></h2>
+              <Form.Control
+                  placeholder="Enter a name"
+                  value={newNickname}
+                  onChange={(e) => setNewNickname(e.target.value)}
+              />
+            </div>
+
+            <div className="form-section">
+              <h2><Form.Label>Your Weight</Form.Label></h2>
+              <Form.Control
+                  type="number"
+                  placeholder="Enter a number"
+                  value={newWeight}
+                  onChange={(e) => setNewWeight(e.target.value)}
+              />
+            </div>
+
+            <div className="form-section">
+              <h2><Form.Label>Sex</Form.Label></h2>
+              <div className="radio-group">
+                <Form.Check
+                    type="radio"
+                    label="Male"
+                    checked={sex === "male"}
+                    onChange={(e) => setSex("male")}
+                />
+                <Form.Check
+                    type="radio"
+                    label="Female"
+                    checked={sex === "female"}
+                    onChange={(e) => setSex("female")}
+                />
+              </div>
+            </div>
+
+            <div className="form-section">
+              <h2><Form.Label>Preferred Lifts</Form.Label></h2>
+              <div className="checkbox-group">
+                <Form.Check
+                    type="checkbox"
+                    label="Bench"
+                    checked={showBench}
+                    onChange={(e) => setShowBench(!showBench)}
+                />
+                <Form.Check
+                    type="checkbox"
+                    label="Squat"
+                    checked={showSquat}
+                    onChange={(e) => setShowSquat(!showSquat)}
+                />
+                <Form.Check
+                    type="checkbox"
+                    label="Deadlift"
+                    checked={showDeadlift}
+                    onChange={(e) => setShowDeadlift(!showDeadlift)}
+                />
+              </div>
+            </div>
+
+            <RangeCollection
+                showBench={showBench}
+                showSquat={showSquat}
+                showDeadlift={showDeadlift}
+                newBench={newBench}
+                setNewBench={setNewBench}
+                newSquat={newSquat}
+                setNewSquat={setNewSquat}
+                newDeadlift={newDeadlift}
+                setNewDeadlift={setNewDeadlift}
             />
-            <Form.Check
-              type="radio"
-              label="Female"
-              checked={sex === "female"}
-              onChange={(e) => setSex("female")}
-            />
-          </div>
+
+            <Button type="submit">Submit</Button>
+          </form>
         </div>
+    );
+  }
 
-        <div className="form-section">
-          <h2><Form.Label>Preferred Lifts</Form.Label></h2>
-          <div className="checkbox-group">
-            <Form.Check
-              type="checkbox"
-              label="Bench"
-              checked={showBench}
-              onChange={(e) => setShowBench(!showBench)}
-            />
-            <Form.Check
-              type="checkbox"
-              label="Squat"
-              checked={showSquat}
-              onChange={(e) => setShowSquat(!showSquat)}
-            />
-            <Form.Check
-              type="checkbox"
-              label="Deadlift"
-              checked={showDeadlift}
-              onChange={(e) => setShowDeadlift(!showDeadlift)}
-            />
-          </div>
-        </div>
-
-        <RangeCollection 
-          showBench={showBench} 
-          showSquat={showSquat} 
-          showDeadlift={showDeadlift}
-          newBench={newBench} 
-          setNewBench={setNewBench}
-          newSquat={newSquat} 
-          setNewSquat={setNewSquat}
-          newDeadlift={newDeadlift} 
-          setNewDeadlift={setNewDeadlift}
-        />
-        
-        <Button type="submit">Submit</Button>
-      </form>
-    </div>
-  );
 };
 
 export default SubmitForm;
