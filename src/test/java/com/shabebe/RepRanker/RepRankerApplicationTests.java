41260e088a907bafb25463f1dee5b8ca4cd7f21c
shabebe11 <salk147@aucklanduni.ac.nz>
2025-07-16 13:34:44 +1200

    Changign backend and settings to fix the backend to put onto aws

--- src/test/java/com/shabebe/RepRanker/RepRankerApplicationTests.java
+++ src/test/java/com/shabebe/RepRanker/RepRankerApplicationTests.java
@@ -2,12 +2,13 @@ package com.shabebe.RepRanker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+// OR use @ActiveProfiles("test")
 class RepRankerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
